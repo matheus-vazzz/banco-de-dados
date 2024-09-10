c10b4eeb813592703c78372846a2d67c0e9d6037
matheus-vazzz <mvaz41322@gmail.com>
2024-09-10 19:17:52 -0300

    Add files via upload

---
+++ create schema.sql
@@ -0,0 +1,8 @@
+create schema `db_senac`;
+create table `db_senac`.`tbl_senac`(
+	`id` int not null auto_increment,
+    `nome` varchar(255) not null,
+    `email` varchar(255) not null,
+    `senha` varchar(255) not null,
+    primary key (`id`)
+);
